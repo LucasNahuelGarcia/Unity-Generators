f2ab263429ff78d2819cc4b109566144b3cd93c0
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2022-11-25 01:18:24 -0300

    added helper to save Matrix4x4 in batches for GPU Instancing

---
+++ Utils/GPUInstancingBatches.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generador
+{
+    public class GPUInstancingBatches
+    {
+        public List<List<Matrix4x4>> Batches
+        {
+            get
+            {
+                return batches;
+            }
+        }
+        private List<List<Matrix4x4>> batches;
+        private const int _BatchesLimit = 1000;
+        private List<Matrix4x4> currentMatrix;
+
+        public GPUInstancingBatches()
+        {
+            batches = new List<List<Matrix4x4>>();
+            currentMatrix = new List<Matrix4x4>();
+            batches.Add(currentMatrix);
+        }
+
+        public void AddMatrix(Matrix4x4 matrix)
+        {
+            currentMatrix.Add(matrix);
+            if (currentMatrix.Count >= _BatchesLimit)
+            {
+                    currentMatrix = new List<Matrix4x4>();
+                    batches.Add(currentMatrix);
+            }
+        }
+    }
+}
